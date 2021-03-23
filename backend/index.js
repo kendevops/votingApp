8fbf588dd3a703cfc31bf4ea6772778b5669808b
Kenneth <kendevops@gmail.com>
2021-03-23 23:20:00 +0100

    created other folders for the server side

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,19 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
-app.get("/", (req, res) => res.send("Hello World"));
+app.get("/", (req, res) => res.json({ Greet: "Hello World" }));
+
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    err: err.message || "something went wrong",
+  });
+});
 
 app.listen(port, console.log(`Server started on port ${port}`));
