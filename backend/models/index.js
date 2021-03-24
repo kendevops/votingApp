c3eb69d187e9187cfc3f72bd47918debd56222cf
Kenneth <kendevops@gmail.com>
2021-03-24 22:16:49 +0100

    Added user and poll

--- backend/models/index.js
+++ backend/models/index.js
@@ -3,3 +3,6 @@ const mongoose = require("mongoose");
 mongoose.set("debug", true);
 mongoose.promise = global.promise;
 mongoose.connect("mongodb://localhost/voteApp");
+
+module.exports.User = require("./user");
+module.exports.Poll = require("./poll");
